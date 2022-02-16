fea9c49953c8a4bb6fceaae9f025e2093203a481
tolluset <dlqud19@gmail.com>
2022-02-16 14:20:55 +0900

    feat: add discord login button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { FaDiscord } from 'react-icons/fa';
 
 const App = () => {
   return (
-    <>
-      <h1>React</h1>
-      <p>How to setting the project</p>
-    </>
+    <div className="flex h-screen items-center justify-center">
+      <button className="m-4 flex items-center gap-2 rounded-sm bg-[#7289da] p-2 text-white transition-all duration-300 ease-in-out hover:bg-black">
+        <FaDiscord size={20} />
+        Login with Discord
+      </button>
+    </div>
   );
 };
 
