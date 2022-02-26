4d41050f33592ea756dfa4ff56a81e3a855ea606
tolluset <dlqud19@gmail.com>
2022-02-27 00:09:03 +0900

    chore: remove console.log

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import DiscordLogin from './components/buttons/DiscordLogin';
 
 const App = () => {
-  console.log(process.env.REACT_APP_DISCORD_OAUTH2_URL);
   return (
     <div className="flex h-screen items-center justify-center">
       <DiscordLogin />
