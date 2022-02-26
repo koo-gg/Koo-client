de4ad9d2ce27a284cc39ed745dd7aa8f02d68932
tolluset <dlqud19@gmail.com>
2022-02-26 21:07:22 +0900

    chore: using react 17 config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,42 +1,45 @@
-import path from "path";
-import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import ESLintPlugin from 'eslint-webpack-plugin';
-import "webpack-dev-server";
+import 'webpack-dev-server';
 
 const config: webpack.Configuration = {
-  mode: "development",
-  entry: "./src/index.tsx",
+  mode: 'development',
+  entry: './src/index.tsx',
+  devServer: {
+    port: 3000,
+  },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         include: path.resolve(__dirname, 'src'),
-        use: ["babel-loader"]
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
         include: path.resolve(__dirname, 'src'),
-        use: ['style-loader', 'css-loader', 'postcss-loader']
-      }
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html",
+      template: 'public/index.html',
     }),
     new CleanWebpackPlugin(),
     new ESLintPlugin({
-      extensions: ['ts', 'tsx']
-    })
+      extensions: ['ts', 'tsx'],
+    }),
   ],
 };
 
