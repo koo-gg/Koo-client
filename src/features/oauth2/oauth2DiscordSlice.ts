d510305b5109950431eec8e108c212eed139cde2
tolluset <dlqud19@gmail.com>
2022-03-02 22:04:16 +0900

    feat: add redux reducer and store

---
+++ src/features/oauth2/oauth2DiscordSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const oauth2DiscordSlice = createSlice({
+  name: 'oauth2Discord',
+  initialState,
+  reducers: {},
+});
+
+export default oauth2DiscordSlice.reducer;
