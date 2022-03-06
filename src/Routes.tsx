56930eabd53cf46323f1ba34c62eddb7daaa242f
tolluset <dlqud19@gmail.com>
2022-03-06 09:58:03 +0900

    temp: showing routing problem in minio

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import DiscordLogin from './components/buttons/DiscordLogin';
+import Detail from './pages/Detail';
 import Home from './pages/Home';
 
 const _Routes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}>
-        <Route index element={<DiscordLogin />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
+      <Route path="detail" element={<Detail />} />
     </Routes>
   );
 };
---
+++ src/pages/Detail.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center bg-default">
+      Detail
+    </div>
+  );
+};
+
+export default Home;
